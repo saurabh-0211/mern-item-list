53a6e51827d1ad6153dceeb3417183c31f9c9f2d
saurabh-0211 <saurabhdubey0211@gmail.com>
2021-07-10 23:32:44 +0530

    version 1.0.0

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 //DB config
-const db = config.get('mongoURI');
+const db = process.env.MONGODB_URI || config.get('mongoURI');
 
 // connect to mongo
 mongoose
